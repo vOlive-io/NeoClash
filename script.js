72cc22a7d4bbcbf7b84867684a3f29a7e85023ca
Olive <141564336+FlamingOlives@users.noreply.github.com>
2024-03-09 18:48:28 -0700

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,4 @@
-//add link forward here
+function openFight() {window.location.replace(window.location.origin + "/fight");}
+function openStore() {window.location.replace(window.location.origin + "/store");}
+function openBank()  {window.location.replace(window.location.origin + "/bank");}
+function openStats() {window.location.replace(window.location.origin + "/stats");}
